7d91b9c13e20c18aec852b671c01fddf926d2ff9
Andrew Yolland <and.t.yoll@gmail.com>
2015-04-09 16:14:25 -0500

    heh. first commit

---
+++ apothecary.js
@@ -0,0 +1,22 @@
+var header = $('header');
+var headerY = 0;
+
+$('.hover').bind('touchstart touchend', function(e) {
+    e.preventDefault();
+    $(this).toggleClass('hover_effect');
+});
+
+window.setInterval(ratchet, 30);
+
+function ratchet(){
+  headerY = (window.scrollY * 0.5) 
+  header.css("transform","translateY(-" + headerY + "px)");
+};
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+});
+
+$('nav li').click(function() {
+  $('html, body').animate({ scrollTop: $($(this.children[0]).attr('href')).offset().top + 'px' }, 500);
+});
